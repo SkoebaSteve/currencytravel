02dbd4d79d9438047e489a6771099dbed8d52003
SkoebaSteve <janssenman@gmail.com>
2016-09-10 18:37:56 +0200

    added new currency list for dropdown and added dropdown that gets data from ap

--- public/app.js
+++ public/app.js
@@ -1,19 +1,29 @@
   // create the module and name it scotchApp
-  var currencyApp = angular.module('CurrencyApp', ['ngRoute']);
+  var currencyApp = angular.module('CurrencyApp', ['ngRoute', 'ngResource']);
 
   // configure our routes
-  currencyApp.config(function($routeProvider) {
+  currencyApp.config(function($routeProvider, $locationProvider) {
     $routeProvider
 
       // route for the home page
       .when('/', {
         templateUrl : 'views/home.html',
         controller  : 'mainController'
-      })
+      });
+
+    // change urls from hashbangs
+    $locationProvider.html5Mode(true);
   });
 
+  currencyApp.factory('ListOfCurrencies', function($resource){
+    return $resource('/api/common-currency', {}, {});
+  })
+
   // create the controller and inject Angular's $scope
-  currencyApp.controller('mainController', function($scope) {
+  currencyApp.controller('mainController', function($scope, ListOfCurrencies) {
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
+    $scope.ListOfCurrencies = ListOfCurrencies.query(function(data){
+    });
+
   });
--- server.js
+++ server.js
@@ -5,11 +5,17 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(express.static('./public'));
+app.use(express.static('./data'));
 
 // all other request point to index
 
+app.get('/api/common-currency', function(req, res){
+  res.sendFile(__dirname + '/data/currencies.json');
+})
+
 app.get('*', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
+
 });
 
 app.listen(process.env.PORT || 3000);
