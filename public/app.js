3ba2a1eecf2983533fd2b1f89fd5441020aada50
SkoebaSteve <janssenman@gmail.com>
2016-09-12 18:22:18 +0200

    displaying list of currencies with exchange rates in view

--- public/app.js
+++ public/app.js
@@ -1,52 +1,53 @@
-  // create the module and name it scotchApp
-  var currencyApp = angular.module('currencyApp', ['ngRoute', 'ngResource']);
+// create the module and name it scotchApp
+var currencyApp = angular.module('currencyApp', ['ngRoute', 'ngResource']);
 
-  // configure our routes
-  currencyApp.config(function($routeProvider, $locationProvider) {
-    $routeProvider
+// configure our routes
+currencyApp.config(function($routeProvider, $locationProvider) {
+  $routeProvider
 
-      // route for the home page
-      .when('/', {
-        templateUrl : 'views/home.html',
-        controller  : 'MainController'
-      })
-      .when('/currency', {
-        templateUrl : 'views/currencies.html',
-        controller  : 'CurrencyController'
-      });
-
-    // change urls from hashbangs
-    $locationProvider.html5Mode(true);
-  });
-
-  // setup factories to talk to the API
-  currencyApp.factory('ListOfCurrencies', function($resource){
-    return $resource('/api/common-currency', {}, {});
-  })
-
-  currencyApp.factory('LiveCurrencies', function($resource){
-    return $resource('/api/current-rate', {}, {});
-  });
-
-  currencyApp.factory('HistoricalCurrencies', function($resource){
-    return $resource('/api/historical-rate', {});
-  });
-
-
-  // create the controller and inject Angular's $scope
-  currencyApp.controller('MainController', function($scope, ListOfCurrencies) {
-
-    $scope.ListOfCurrencies = ListOfCurrencies.query(function(data){
+    // route for the home page
+    .when('/', {
+      templateUrl : 'views/home.html',
+      controller  : 'MainController'
+    })
+    .when('/currency', {
+      templateUrl : 'views/currencies.html',
+      controller  : 'CurrencyController'
     });
 
-    $scope.findCurrencies = function(form){
-      console.log(form);
-    }
+  // change urls from hashbangs
+  $locationProvider.html5Mode(true);
+});
 
+// setup factories to talk to the API
+currencyApp.factory('ListOfCurrencies', function($resource){
+  return $resource('/api/common-currency', {}, {});
+});
+
+currencyApp.factory('LiveCurrencies', function($resource){
+  return $resource('/api/current-rate', {}, {});
+});
+
+currencyApp.factory('HistoricalCurrencies', function($resource){
+  return $resource('/api/historical-rate', {});
+});
+
+
+// create the controller and inject Angular's $scope
+currencyApp.controller('MainController', function($scope, ListOfCurrencies) {
+
+  $scope.ListOfCurrencies = ListOfCurrencies.query(function(data){
   });
 
-  // create the controller and inject Angular's $scope
-  currencyApp.controller('CurrencyController', function($scope, ListOfCurrencies, LiveCurrencies, HistoricalCurrencies) {
+  $scope.findCurrencies = function(form){
+    console.log(form);
+  }
 
-  });
+});
+
+// create the controller and inject Angular's $scope
+currencyApp.controller('CurrencyController', function($scope, ListOfCurrencies, LiveCurrencies, HistoricalCurrencies) {
+  $scope.CurrentExchangeRate = LiveCurrencies.query(function(data){});
+  $scope.HistoricalExchangeRate = HistoricalCurrencies.query(function(data){});
+});
 
